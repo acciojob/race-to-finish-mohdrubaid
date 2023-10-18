28de27af4ee72c72b484457e7ceefb50bc06fb11
mohdrubaid <125140108+mohdrubaid@users.noreply.github.com>
2023-10-18 11:00:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 window.promises = [];
 
+
 // Do not change the code above this
 // add your promises to the array `promises`
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // reject("Sorry no data found");
+    resolve("Here is your data...");
+  }, 2000);
+});
+
+// console.log("Promise", myPromise, new Date());
+
+// myPromise
+//   .then((data) => {
+//     console.log("DATA....", data);
+//   })
+//   .catch((err) => {
+//     console.log("CATCHING....", err);
+//   });
